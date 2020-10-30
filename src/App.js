dc4c53adb728e196ef234425c2518646fd61abe7
new <new@pop-os.localdomain>
2020-10-30 11:05:20 -0700

    html coding for main page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Technologies from './Technologies';
-import Header from './Header';
-import Footer from './Footer';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
-      <Footer />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://s2.logaster.com/static/v3/img/products/logo.png' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://media.timeout.com/images/100541963/image.jpg' />
+        </div>
+        <div>
+          ava+description
+        </div>
+        <div>
+          my posts
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <div>
-            it-kamasutra
-        </div>
-    );
-}
-
-export default Footer;
