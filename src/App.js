59560c9479c6ef9d2d1f9378b9b5f9ac07e772c7
new <new@pop-os.localdomain>
2020-10-30 17:49:57 -0700

    Moved parts to separate components

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className='app-wrapper'>
-      <header className='header'>
-        <img src='https://s2.logaster.com/static/v3/img/products/logo.png' />
-      </header>
-      <nav className='nav'>
-        <div>
-          <a>Profile</a>
-        </div>
-        <div>
-          <a>Messages</a>
-        </div>
-        <div>
-          <a>News</a>
-        </div>
-        <div>
-          <a>Music</a>
-        </div>
-        <div>
-          <a>Settings</a>
-        </div>
-      </nav>
-      <div className='content'>
-        <div>
-          <img src='https://media.timeout.com/images/100541963/image.jpg' />
-        </div>
-        <div>
-          ava+description
-        </div>
-        <div>
-          my posts
-          <div>
-            new post
-          </div>
-          <div>
-            <div>
-              post 1
-            </div>
-            <div>
-              post 2
-            </div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src='https://media.timeout.com/images/100541963/image.jpg' />
+      </div>
+      <div>
+        ava+description
+    </div>
+      <div>
+        my posts
+      <div>
+          new post
+      </div>
+        <div>
+          <div>
+            post 1
+        </div>
+          <div>
+            post 2
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
