278e0a0286b701507022dc71ba168fa64c520741
new <new@pop-os.localdomain>
2020-10-30 20:33:35 -0700

    move css styles to css modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={s.content}>
       <div>
         <img src='https://media.timeout.com/images/100541963/image.jpg' />
       </div>
       <div>
         ava+description
-    </div>
+      </div>
       <div>
         my posts
-      <div>
-          new post
-      </div>
         <div>
-          <div>
+          new post
+        </div>
+        <div classname={s.posts}>
+          <div className={s.item}>
             post 1
-        </div>
-          <div>
+          </div>
+          <div className={s.item}>
             post 2
-        </div>
+          </div>
         </div>
       </div>
     </div>
