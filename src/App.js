48e8bc6009e28b629465b4f5911f053f53c7f688
new <new@pop-os.localdomain>
2020-11-02 08:08:02 -0800

    added state to app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dialogs from './components/Dialogs/Dialogs';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = (props) => {
+
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
@@ -14,12 +15,13 @@ const App = (props) => {
         <Navbar />
         {/* <Profile /> */}
         <div className='app-wrapper-content'>
-          <Route path="/dialogs" render={() => <Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
-          <Route path="/profile" render={() => <Profile posts={props.posts}/>} />
+          <Route path="/dialogs"
+            render={() => <Dialogs state={props.state.dialogsPage} />} />
+          <Route path="/profile"
+            render={() => <Profile state={props.state.profilePage} />} />
         </div>
       </div>
     </BrowserRouter>
-
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,67 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let dialogs = [
-  {
-    id: 1,
-    name: 'Dimych'
-  },
-  {
-    id: 2,
-    name: 'Andrey'
-  },
-  {
-    id: 3,
-    name: 'Sveta'
-  },
-  {
-    id: 4,
-    name: 'Sasha'
-  },
-  {
-    id: 5,
-    name: 'Viktor'
-  }
-];
-
-let messages = [
-  {
-    id: 1,
-    message: "Hi"
-  },
-  {
-    id: 2,
-    message: "How is your it-kamasutra?"
-  },
-  {
-    id: 3,
-    message: "Yo"
-  },
-  {
-    id: 4,
-    message: "Yo"
-  }
-];
-
-
-let posts = [
-  {
-    id: 1,
-    message: 'Hi, how are you?',
-    likesCount: 12
-  },
-  {
-    id: 2,
-    message: 'Its my first post',
-    likesCount: 10
-  },
-];
+import state from './redux/state';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App messages={messages} dialogs={dialogs} posts={posts} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
