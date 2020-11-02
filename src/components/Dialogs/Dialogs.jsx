c98791cf6a9cb43bf48de05775be7dc03479ef55
new <new@pop-os.localdomain>
2020-11-01 20:44:10 -0800

    dialogs styling

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogItems}>
+        <div class={s.dialog + ' ' + s.active}>
+          <NavLink to="/dialogs/1">Dimych</NavLink>
+        </div>
+        <div class={s.dialog}>
+          <NavLink to="/dialogs/2">Andrey</NavLink>
+        </div>
+        <div class={s.dialog}>
+          <NavLink to="/dialogs/3">Sveta</NavLink>
+        </div>
+        <div class={s.dialog}>
+          <NavLink to="/dialogs/4">Sasha</NavLink>
+        </div>
+        <div class={s.dialog}>
+          <NavLink to="/dialogs/5">Viktor</NavLink>
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>
+          Hi
+          </div>
+        <div className={s.message}>
+          How is your it-kamasutra?
+          </div>
+        <div className={s.message}>
+          Yo
+        </div>
+      </div>
     </div>
   )
 }
