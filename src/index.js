09339426a0024deb0840548b412b70268eafeda1
new <new@pop-os.localdomain>
2020-11-02 22:51:41 -0800

    minor refactoring

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, { addPost } from './redux/state';
-import { rerenderEntireTree } from './render';
+import state, { addPost, updateNewPostText, subscribe } from './redux/state';
 
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 rerenderEntireTree(state);
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import { rerenderEntireTree } from "../render";
+let rerenderEntireTree = () => {
+  console.log('State changed')
+}
 
 let state = {
   profilePage: {
@@ -63,7 +65,7 @@ let state = {
 
 window.state = state;
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -73,9 +75,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
 }
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
+} 
 
 export default state;
