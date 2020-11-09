f20679562e66626a8307c66c382ddbc7d764cb96
new <new@pop-os.localdomain>
2020-11-09 09:49:48 -0800

    login authorization in header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import React from 'react';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Route } from 'react-router-dom';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import UsersContainer from './components/Users/UsersContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
 
 const App = (props) => {
   return (
     <BrowserRouter>
       <div className='app-wrapper'>
-        <Header />
+        <HeaderContainer />
         <Navbar />
         {/* <Profile /> */}
         <div className='app-wrapper-content'>
--- src/redux/store.js
+++ src/redux/store.js
@@ -84,4 +84,4 @@ let store = {
 }
 
 export default store;
-window.store = store;
+//window.store = store;
