afd483e251eaa299af4a82fdd6d0cc5fd41d2b99
new <new@pop-os.localdomain>
2020-11-10 14:45:11 -0800

    add thunks for follow/unfollow feature

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import s from './users.module.css';
 import userPhoto from '../../assets/images/user_avatar.jpg'
 import { NavLink } from 'react-router-dom';
-import { usersAPI } from '../../api/api';
 
 let Users = (props) => {
 
@@ -39,27 +38,18 @@ let Users = (props) => {
               <div>
                 {
                   u.followed
-                    ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                      props.toggleFollowingProgress(true, u.id);
-                      usersAPI.unfollow(u.id)
-                        .then(response => {
-                          if (response.data.resultCode === 0) {
-                            props.unfollow(u.id);
-                          }
-                          props.toggleFollowingProgress(false, u.id);
-
-                        });
-                    }}>Unfollow</button>
-                    : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                      props.toggleFollowingProgress(true, u.id);
-                      usersAPI.follow(u.id)
-                        .then(response => {
-                          if (response.data.resultCode === 0) {
-                            props.follow(u.id);
-                          }
-                          props.toggleFollowingProgress(false, u.id);
-                        });
-                    }}>Follow</button>
+                    ? <button
+                      disabled={props.followingInProgress.some(id => id === u.id)}
+                      onClick={() => { props.unfollow(u.id); }}
+                    >
+                      Unfollow
+                        </button>
+                    : <button
+                      disabled={props.followingInProgress.some(id => id === u.id)}
+                      onClick={() => { props.follow(u.id); }}
+                    >
+                      Follow
+                    </button>
                 }
               </div>
             </span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -85,14 +85,14 @@ const usersReducer = (state = initialState, action) => {
   }
 }
 
-export const follow = (userId) => (
+export const followSuccess = (userId) => (
   {
     type: FOLLOW,
     userId
   }
 );
 
-export const unfollow = (userId) => (
+export const unfollowSuccess = (userId) => (
   {
     type: UNFOLLOW,
     userId
@@ -143,6 +143,33 @@ export const getUsers = (currentPage, pageSize) => {
         dispatch(toggleIsFetching(false));
         dispatch(setUsers(data.items));
         dispatch(setTotalUsersCount(data.totalCount));
+        dispatch(setCurrentPage(currentPage));
+      });
+  }
+}
+
+export const follow = (userId) => {
+  return (dispatch) => {
+    dispatch(toggleFollowingProgress(true, userId));
+    usersAPI.follow(userId)
+      .then(response => {
+        if (response.data.resultCode === 0) {
+          dispatch(followSuccess(userId));
+        }
+        dispatch(toggleFollowingProgress(false, userId));
+      });
+  }
+}
+
+export const unfollow = (userId) => {
+  return (dispatch) => {
+    dispatch(toggleFollowingProgress(true, userId));
+    usersAPI.unfollow(userId)
+      .then(response => {
+        if (response.data.resultCode === 0) {
+          dispatch(unfollowSuccess(userId));
+        }
+        dispatch(toggleFollowingProgress(false, userId));
       });
   }
 }
