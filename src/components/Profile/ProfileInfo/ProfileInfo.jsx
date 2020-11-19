c3e7700980414dd180696c4806cddd7cdbdd881c
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-11-19 12:25:47 -0800

    add set state hooks for profile status component

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
-import ProfileStatus from './ProfileStatus/ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatus/ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -14,7 +14,7 @@ const ProfileInfo = (props) => {
           className={s.contentImage}
           src='https://media.timeout.com/images/100541963/image.jpg'
           alt='' />
-        <ProfileStatus 
+        <ProfileStatusWithHooks 
           status={props.status} 
           updateStatus={props.updateStatus} />
       </div>
---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+
+const ProfileStatusWithHooks = (props) => {
+
+  let [editMode, setEditMode] = useState(false);
+  let [status, setStatus] = useState(props.status);
+
+
+  const activateEditMode = () => {
+    setEditMode(true);
+  }
+
+  const deactivateEditMode = () => {
+    setEditMode(false);
+    props.updateStatus(status);
+  }
+
+  const onStatusChange = (e) => {
+    setStatus(e.currentTarget.value);
+  }
+
+  return (
+    <div>
+      { !editMode &&
+        <div>
+          <span
+            onDoubleClick={activateEditMode}>
+            {props.status || '---'}
+          </span>
+        </div>
+      }
+      { editMode &&
+        <div>
+          <div>
+            <input
+              autoFocus={true}
+              onBlur={deactivateEditMode}
+              onChange={onStatusChange}
+              value={status} />
+          </div>
+        </div>
+      }
+    </div>
+  );
+}
+
+export default ProfileStatusWithHooks;
