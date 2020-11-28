79da64f576b334e8f74593d4dcf2ccef82c736e5
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2020-11-28 00:38:43 -0800

    captcha url for incorrect login attempts

--- src/api/api.js
+++ src/api/api.js
@@ -1,19 +1,17 @@
-import * as Axios from 'axios';
+import * as Axios from "axios";
 
 const instance = Axios.create({
   withCredentials: true,
-  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
   headers: {
-    'API-KEY': '3d1ff9da-8bc0-4533-ba31-4c93b3db5b95'
-  }
+    "API-KEY": "3d1ff9da-8bc0-4533-ba31-4c93b3db5b95",
+  },
 });
 
 export const usersAPI = {
-
   getUsers(currentPage = 1, pageSize = 10) {
     let url = `users?page=${currentPage}&count=${pageSize}`;
-    return instance.get(url)
-      .then(response => response.data);
+    return instance.get(url).then((response) => response.data);
   },
 
   follow(userId) {
@@ -24,66 +22,63 @@ export const usersAPI = {
   unfollow(userId) {
     let url = `follow/${userId}`;
     return instance.delete(url);
-  }
-
-}
+  },
+};
 
 export const profileAPI = {
-
   getProfile(userId) {
     let url = `profile/${userId}`;
-    return instance.get(url)
-      .then(response => {
-        return response.data;
-      });
+    return instance.get(url).then((response) => {
+      return response.data;
+    });
   },
 
   getStatus(userId) {
     let url = `profile/status/${userId}`;
-    return instance.get(url).then(status => {
+    return instance.get(url).then((status) => {
       return status;
     });
   },
 
   updateStatus(status) {
     let url = `profile/status`;
-    return instance.put(url, { status: status })
-      .then(status => {
-        return status;
-      });
+    return instance.put(url, { status: status }).then((status) => {
+      return status;
+    });
   },
 
   savePhoto(photoFile) {
     let formData = new FormData();
-    formData.append('image', photoFile);
-    return instance.put(
-      'profile/photo',
-      formData, 
-      { 
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      }  
-    );
+    formData.append("image", photoFile);
+    return instance.put("profile/photo", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
   },
 
   saveProfile(profile) {
-    return instance.put('profile', profile );
-  }
-
-}
+    return instance.put("profile", profile);
+  },
+};
 
 export const authAPI = {
   me() {
     let url = `auth/me`;
-    return instance.get(url)
+    return instance.get(url);
   },
 
-  login(email, password, rememberMe) {
-    return instance.post('auth/login', { email, password, rememberMe });
+  login(email, password, rememberMe, captcha = null) {
+    return instance.post("auth/login", { email, password, rememberMe, captcha });
   },
 
   logout() {
-    return instance.delete('auth/login');
-  }
-}
+    return instance.delete("auth/login");
+  },
+};
+
+export const securityAPI = {
+  getCaptchaUrl() {
+    return instance.get(`security/get-captcha-url`);
+  },
+};
